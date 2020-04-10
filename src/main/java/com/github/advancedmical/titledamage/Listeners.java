47dc03a1b6953242a084c0f9de27cfb2fd631eda
AdvancedMical <micalhl@sina.cn>
2020-04-10 12:03:06 +0800

    整理类的结构

---
+++ src/main/java/com/github/advancedmical/titledamage/Listeners.java
@@ -0,0 +1,37 @@
+package com.github.advancedmical.titledamage;
+
+import org.bukkit.Sound;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Listeners implements Listener {
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event){
+        if (event.getEntityType() == EntityType.PLAYER){
+            Player player = (Player) event.getEntity();
+                    if (player.getHealth() < 10.0D){
+                        player.sendTitle(JavaPlugin.getPlugin(Main.class).getConfig().getString("bufittitle"), null, 10, 50, 20);
+                    }
+                    if (player.getHealth() < 4.0D){
+                        player.sendTitle(JavaPlugin.getPlugin(Main.class).getConfig().getString("weititle"), null, 10, 50, 20);
+                    }
+                }
+        }
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent e) {
+        double random = Math.random();
+        Player p = e.getPlayer();
+        if (random < 0.01D) {
+            p.sendTitle(JavaPlugin.getPlugin(Main.class).getConfig().getString("restitle"), null, 10, 50, 20);
+        } else {
+            p.sendTitle(JavaPlugin.getPlugin(Main.class).getConfig().getString("dietitle"), null, 10, 50, 20);
+        }
+        p.playSound(p.getLocation(), Sound.ENTITY_CHICKEN_HURT, 100.0F, 0.0F);
+    }
+    }
--- src/main/java/com/github/advancedmical/titledamage/Main.java
+++ src/main/java/com/github/advancedmical/titledamage/Main.java
@@ -1,7 +1,6 @@
 package com.github.advancedmical.titledamage;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.Plugin;
 
@@ -10,8 +9,7 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         super.onEnable();
-        Bukkit.getPluginManager().registerEvents((Listener)new DamageListener(), (Plugin)this) ;
-        Bukkit.getPluginManager().registerEvents((Listener)new RespawnListener(), (Plugin)this);
+        Bukkit.getPluginManager().registerEvents(new Listeners(), (Plugin)this) ;
         saveDefaultConfig();
     }
 
